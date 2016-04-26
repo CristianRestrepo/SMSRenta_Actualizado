a6b12d7533bac70f8155ad7e232fa5d61022fce8
johanncanon <johanncanon@gmail.com>
2016-04-26 15:38:22 -0500

    arreglos varios

--- src/java/com/planit/smsrenta/metodos/SendEmail.java
+++ src/java/com/planit/smsrenta/metodos/SendEmail.java
@@ -497,7 +497,7 @@ public class SendEmail {
             // a donde se envia
             message.addRecipient(
                     Message.RecipientType.TO,
-                    new InternetAddress("desarrollo@smsrenta.com.co"));
+                    new InternetAddress("desarrollo@planit.com.co"));
             message.setSubject("Mensaje html prueba");
             String messageHtml = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n"
                     + "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n"
